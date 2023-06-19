793a160f6badb76cc57ac8a0f3e70cf9c9333682
Naatchi <nataliepahwa@gmail.com>
2023-06-19 12:41:23 -0400

    fixed autoprefixer warn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,10 @@ export default function Home() {
             page_name='Home'
             page_ref='/'
             content={
-                <section id='hero' className='flex'>
-                    <img
-                        title={database.images.profiles.index_picture.title}
-                        alt={database.images.profiles.index_picture.alt}
-                        src={database.images.profiles.index_picture.src}
-                    />
-                    <div> 
-                    </div>
-                </section> 
+                <>
+                    this is the content <br/>
+                    
+                </>
             }
         />
     )
