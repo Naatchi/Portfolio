ffcf5bd182834c6be9cfac66f582f334932cf3af
Naatchi <nataliepahwa@gmail.com>
2023-06-21 13:13:55 -0400

    fixed wave styling issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Wave, WaveArray } from "@/components/waves";
 import { database } from "@/data/database";
+import { Colors } from "@/styles/colors";
 import styles from '@/styles/index.module.scss'
 
 export default function Home() {
-    <>
-    
-    </>
+    return (
+        <div id='site-background' className='bg-siteBlack'>
+            <section 
+                id='landing'
+                className='h-screen w-auto'>
+                <Wave
+                    id='wave'
+                    svg={WaveArray[0]} 
+                    fill={Colors.backgroundPurple}/>
+            </section>
+        </div>
+    )
 }
